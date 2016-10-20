3095a2873e71db367b44181c0bd3847c38c89d17
rienafairefr <rienafairefr@gmail.com>
2016-10-20 13:41:10 +0200

    def test_sync [ci-cron]

--- test_sync/test_sync.py
+++ test_sync/test_sync.py
@@ -2,7 +2,10 @@ from pynYNAB.Client import clientfromargs
 from pynYNAB.scripts.config import parser
 
 # used to ping the nYNAB API to check that the sync works
+def test_sync():
+    args = parser.parse_known_args()[0]
+    client = clientfromargs(args)
+    client.sync()
 
-args = parser.parse_known_args()[0]
-client = clientfromargs(args)
-client.sync()
+if __name__ == '__main__':
+    test_sync()
